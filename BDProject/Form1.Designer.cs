0d17be4edf83e73df2d1415d09749337dcbd3dab
Cosmin Ionite <cosmin.ionite@gmail.com>
2017-12-02 23:56:41 +0200

    Conectare BD

--- BDProject/Form1.Designer.cs
+++ BDProject/Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace BDProject
 {
-    partial class Form1
+    partial class LogIn
     {
         /// <summary>
         /// Required designer variable.
@@ -28,46 +28,95 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.button1 = new System.Windows.Forms.Button();
-            this.dataGridView1 = new System.Windows.Forms.DataGridView();
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.Username_Box = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.Password_Box = new System.Windows.Forms.TextBox();
+            this.Login_Button = new System.Windows.Forms.Button();
+            this.Exit_Button = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
-            // button1
+            // Username_Box
             // 
-            this.button1.Location = new System.Drawing.Point(166, 12);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "button1";
-            this.button1.UseVisualStyleBackColor = true;
+            this.Username_Box.Location = new System.Drawing.Point(144, 56);
+            this.Username_Box.Name = "Username_Box";
+            this.Username_Box.Size = new System.Drawing.Size(206, 20);
+            this.Username_Box.TabIndex = 0;
             // 
-            // dataGridView1
+            // label1
             // 
-            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Location = new System.Drawing.Point(12, 76);
-            this.dataGridView1.Name = "dataGridView1";
-            this.dataGridView1.Size = new System.Drawing.Size(240, 150);
-            this.dataGridView1.TabIndex = 1;
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(58, 59);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(38, 17);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "User";
             // 
-            // Form1
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(58, 116);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(49, 17);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Parola";
+            // 
+            // Password_Box
+            // 
+            this.Password_Box.Location = new System.Drawing.Point(144, 113);
+            this.Password_Box.Name = "Password_Box";
+            this.Password_Box.PasswordChar = '*';
+            this.Password_Box.Size = new System.Drawing.Size(206, 20);
+            this.Password_Box.TabIndex = 3;
+            // 
+            // Login_Button
+            // 
+            this.Login_Button.Location = new System.Drawing.Point(144, 171);
+            this.Login_Button.Name = "Login_Button";
+            this.Login_Button.Size = new System.Drawing.Size(75, 23);
+            this.Login_Button.TabIndex = 4;
+            this.Login_Button.Text = "Login";
+            this.Login_Button.UseVisualStyleBackColor = true;
+            this.Login_Button.Click += new System.EventHandler(this.Login_Button_Click);
+            // 
+            // Exit_Button
+            // 
+            this.Exit_Button.Location = new System.Drawing.Point(275, 171);
+            this.Exit_Button.Name = "Exit_Button";
+            this.Exit_Button.Size = new System.Drawing.Size(75, 23);
+            this.Exit_Button.TabIndex = 5;
+            this.Exit_Button.Text = "Exit";
+            this.Exit_Button.UseVisualStyleBackColor = true;
+            // 
+            // LogIn
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(284, 261);
-            this.Controls.Add(this.dataGridView1);
-            this.Controls.Add(this.button1);
-            this.Name = "Form1";
-            this.Text = "Form1";
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ClientSize = new System.Drawing.Size(374, 227);
+            this.Controls.Add(this.Exit_Button);
+            this.Controls.Add(this.Login_Button);
+            this.Controls.Add(this.Password_Box);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.Username_Box);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "LogIn";
+            this.Text = "LogIn";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
-        private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.TextBox Username_Box;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox Password_Box;
+        private System.Windows.Forms.Button Login_Button;
+        private System.Windows.Forms.Button Exit_Button;
     }
 }
 
--- BDProject/Form1.cs
+++ BDProject/Form1.cs
@@ -7,14 +7,34 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Data;
+using System.Data.SqlClient;
 
 namespace BDProject
 {
-    public partial class Form1 : Form
+    public partial class LogIn : Form
     {
-        public Form1()
+        public static SqlConnection connection;
+        public LogIn()
         {
             InitializeComponent();
+            connection = new SqlConnection();
         }
+
+        private void Login_Button_Click(object sender, EventArgs e)
+        {
+            string Username = Username_Box.Text.ToString();
+            string Password = Password_Box.Text.ToString();
+            connection.ConnectionString = "Server=.;DataBase=Cabinet_Medical;Trusted_Connection=false;User=" + Username + ";Password=" + Password + ";";
+            connection.Open();
+            Console.WriteLine(connection.ServerVersion.ToString());
+           // textBox1.AppendText(connection.ServerVersion.ToString());
+            connection.Close();
+
+            Hide();
+            Form2 Test = new Form2();
+
+        }
+       
     }
 }
